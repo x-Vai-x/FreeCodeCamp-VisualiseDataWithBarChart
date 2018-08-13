94890bb2eb54613b8b4ed49c3348e1ac7e52d0b5
Vashali <vaishali2016@outlook.com>
2018-08-13 17:16:22 +0100

    Br graph done

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const path=require('path')
 const app=express()
-
+const {XMLHttpRequest} = require("xmlhttprequest")
 
 app.set('view engine', 'ejs')
 app.set('views', path.resolve(__dirname))
